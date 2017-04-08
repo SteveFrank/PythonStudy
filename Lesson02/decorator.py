788568e935f11eb4c868cf6fffbc8da3a9e5423f
沐风-Qian <yangqian20105@qq.com>
2017-04-08 22:39:48 +0800

    Python Study

--- Lesson02/decorator.py
+++ Lesson02/decorator.py
@@ -7,8 +7,21 @@ def log(func):
         print 'ending calling', func.__name__
     return wrapper
 
-def hello():
-    print 'hello'
+def log_(func):
+    def wrapper(name):
+        print 'before calling ', func.__name__
+        func(name)
+        print 'ending calling', func.__name__
+    return wrapper
+
+@log
+def hello_function_1():
+    print 'hello_1'
+@log_
+def hello_function_2(name):
+    print 'hello_2', name
+
 
 if __name__ == '__main__':
-    hello()
+    # hello_function_1()
+    hello_function_2('input name')
