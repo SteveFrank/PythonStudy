3a15fdcefc94695751174f051e77f6b7ffde6780
沐风-Qian <yangqian20105@qq.com>
2017-04-09 18:04:00 +0800

    Python Study

--- Lesson02/decorator.py
+++ Lesson02/decorator.py
@@ -14,14 +14,27 @@ def log_(func):
         print 'ending calling', func.__name__
     return wrapper
 
+def log__(func):
+    def wrapper(*args, **kvargs):
+        print 'before calling ', func.__name__
+        print 'args : ' , args , ' , kvargs : ' , kvargs
+        func(*args, **kvargs)
+        print 'ending calling', func.__name__
+    return wrapper
+
 @log
 def hello_function_1():
     print 'hello_1'
 @log_
 def hello_function_2(name):
     print 'hello_2', name
-
+@log__
+def hello_function_3(name, age):
+    print 'name : ' , name, ' age : ' , age
 
 if __name__ == '__main__':
     # hello_function_1()
-    hello_function_2('input name')
+    # hello_function_2('input name')
+    hello_function_3('nowcoder', 12)
+    print '==========================='
+    hello_function_3(name='nowcoder', age=12)
